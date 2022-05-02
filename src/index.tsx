9dbeeea591756b07406cf36fa09bc4ab0626bfe9
zhaofourInPrague <m1325679285@163.com>
2022-05-02 22:47:24 +0800

    jsx的tsconfig以及编译相关

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
-console.log('hello');
-// const x: 1;
-// const n: number = '123';
+import React, { ReactElement, DOMElement, DetailedReactHTMLElement } from 'react';
+import ReactDOM from 'react-dom';
+
+// <h1>hello</h1>会变成下面 tsconfig.js 中jsx: react
+let element = React.createElement('h1', {className:'title'}, 'hello');
+
+// <h1>hello</h1>
+ReactDOM.render(element, document.getElementById('root'));
